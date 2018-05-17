38877d62d159d405b22fb437ff31456926cbf5c7
s3baRr00 <sebastianrilo@gmail.com>
2018-05-17 11:11:18 -0300

    vectores

--- FuncionesPunteros/main.c
+++ FuncionesPunteros/main.c
@@ -3,7 +3,7 @@
 
 void intercambiar(int num1, int num2);
 void intercambiarConPunteros(int *num1, int *num2);
-
+void ponerEn0(int *num1);
 
 int main()
 {
@@ -11,6 +11,7 @@ int main()
     int edadDos;
     edadUno=22;
     edadDos=99;
+    ponerEn0(&edadDos);
     intercambiarConPunteros(&edadUno, &edadDos); //siempre que se quiera trabajar con la direccion de memoria
     printf("\n edad 1: %d", edadUno);
      printf("\n edad 2: %d", edadDos);
@@ -42,6 +43,13 @@ int aux;
     aux=*num1;
     *num1=*num2;
     *num2=aux;
+
+
+}
+
+
+void ponerEn0(int *num1){
+*num1=0;
 
 
 }
