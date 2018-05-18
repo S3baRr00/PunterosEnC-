d700a614bfc6d0dc6a36f6e8f896c44f3eb22175
s3baRr00 <sebastianrilo@gmail.com>
2018-05-18 10:14:50 -0300

    ->

--- estructurasyPunteros/main.c
+++ estructurasyPunteros/main.c
@@ -10,7 +10,7 @@ char b;
 
 int main()
 {
-   dato d;
+dato d;
    d.a=5;
    d.b='<';
 
@@ -18,12 +18,31 @@ int main()
 
    pdato = &d;
 
-   printf("%d--%d\n",&d, &d.a); //devuelve la direcc de memoria
+   printf("\n%d----%c\n", pdato->a, pdato->b); // la mejor forma de mostrar los valores de una estruct a travez de puntero
+   //'->' reemplaza '*','()' y '.'
+   printf("\n%d--%d\n",&d, &d.a); //devuelve la direcc de memoria
 
-   printf("%d----%c\n",(*pdato).a, (*pdato).b); //devuelve los valores de la direc de memoria de esos datos en struct
+   printf("\n%d----%c\n",(*pdato).a, (*pdato).b); //devuelve los valores de la direc de memoria de esos datos en struct
+   // se pone entre parentesis porque el valor de acceso(.) tiene prioridad sobre el valor de inacceso(*)
+   //si no le ponemos parentesis  la computadora va a entender que nos referimos al puntero como una strecutura cosa que no es por lo que nos da error
 
-   printf("TAM: %d\n", sizeof(dato));//sizeoff devuelve el tamaño de un tipo de dato //para que corra en todos los sistemas operativos se usa un sizeof ya que el peso de dato cambia
+   printf("\nTAM: %d\n", sizeof(dato));//sizeoff devuelve el tamaño de un tipo de dato //para que corra en todos los sistemas operativos se usa un sizeof ya que el peso de dato cambia
 
-   printf("a= %d y b= %c", d.a, d.b); //devuelve los valores de las variables dentro de la struct
-    return 0;
+   printf("\na= %d y b= %c\n", d.a, d.b); //devuelve los valores de las variables dentro de la struct
+
+
+
+
+int i;
+dato lista[2]={{1,'a'},{1,'b'}};
+dato* plista;
+plista=lista;
+
+   for(i=0;i<2;i++){
+    //printf("\n%d---%c\n", (*(plista+i)).a , (*(plista+i)).b ); //muestra los valores de una estructura dentro de un for. aunque no aconsejable
+    printf("\n%d---%c", (plista+i)->a , (plista+i)->b );                                                        //'->' reemplaza '*','()' y '.'
+   }
+
+
+return 0;
 }
